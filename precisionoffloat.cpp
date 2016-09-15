c30c16c0fcd9feb501aeaf2e4f0ab94ccf1e6efb
qmhedging <qmhedging@gmail.com>
2016-09-15 21:09:16 +0800

    code 160915-2

---
+++ precisionoffloat.cpp
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+main()
+
+
+{
+double n;
+
+printf("please input a number with at least 3 numbers after the decimal point :");
+
+scanf("%lf",&n);
+
+printf("so the number we take is n = %.2lf",n);
+
+scanf("%f",&n);
+
+}
